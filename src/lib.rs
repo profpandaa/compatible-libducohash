f8658ab84c6db52a3cb2f595116946020acf7d8f
~panda <30387483+profpandaa@users.noreply.github.com>
2024-10-11 23:12:22 +0200

    Optimized lib.rs
    Bumped the hashrate from 8.8/8.9 MH/s on one thread to roughly 10.6 - 11 MH/s on one thread on my machine

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use pyo3::prelude::*;
-use sha1::Sha1;
 use std::time::Duration;
+use sha1_smol::Sha1;
 
 #[pyclass]
 #[derive(Clone)]
@@ -17,25 +17,43 @@ impl DUCOHasher {
 
     #[allow(non_snake_case)] // recommend changing (needs to be changed in miners, too)
     pub fn DUCOS1(&mut self, expected_hash: &[u8], diff: u128, eff: u64) -> u128 {
-        for nonce in 0..(100*diff+1) {
-            let mut temp_hasher = self.hasher.clone();
+        let mut buffer = itoa::Buffer::new();
+        if eff != 0 // this really is a micro-optimization
+        {
+            for nonce in 0..(100*diff+1) {
+                let mut temp_hasher = self.hasher.clone();
 
-            temp_hasher.update(nonce.to_string().as_bytes());
-            let result = temp_hasher.digest().bytes();
+                let str = buffer.format(nonce);
+                temp_hasher.update(str.as_bytes());
 
-            if eff != 0 {
+
                 if nonce % 5000 == 0 {
                     std::thread::sleep(Duration::new(eff/100, 0))
                 }
-            }
 
-            if result == expected_hash {
-                self.hasher.reset();
-                return nonce;
+                if temp_hasher.digest().bytes() == expected_hash {
+                    self.hasher.reset();
+                    return nonce;
+                }
+
             }
-            
         }
-        return 0;
+        else
+        {
+            for nonce in 0..(100*diff+1) {
+                let mut temp_hasher = self.hasher.clone();
+
+                let str = buffer.format(nonce);
+                temp_hasher.update(str.as_bytes());
+
+                if temp_hasher.digest().bytes() == expected_hash {
+                    self.hasher.reset();
+                    return nonce;
+                }
+
+            }
+        }
+        0
     }
 
 }
